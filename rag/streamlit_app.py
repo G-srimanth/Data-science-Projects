fb7490a725c9759f73f6ca93ef9d69e02c54fa6c
srimanth Gundla <154001408+G-srimanth@users.noreply.github.com>
2024-12-10 12:11:16 +0530

    chatbot

---
+++ rag/streamlit_app.py
@@ -0,0 +1,189 @@
+import streamlit as st
+import os
+import google.generativeai as genai
+from langchain.vectorstores import FAISS
+from langchain.document_loaders import PyMuPDFLoader,WebBaseLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+#from langchain.chains.question_answering import load_qa_chain
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_experimental.agents import create_csv_agent
+#from langchain.chains import RetrievalQA
+from langchain_google_genai import ChatGoogleGenerativeAI,GoogleGenerativeAIEmbeddings
+from langchain_core.output_parsers import StrOutputParser
+from langchain.memory import ConversationBufferMemory
+import warnings
+from langchain_core.runnables import RunnablePassthrough
+import tempfile
+warnings.filterwarnings('ignore')
+
+st.title('Multiple Chat bot')
+#key = st.secrets()
+#genai.configure(api_key=key)
+#google_key = st.secrets['google']
+key = st.secrets['google']
+
+chat_type = st.sidebar.selectbox(label="select a bot",options=['Data Science Ai Assitant','Chat with pdf','Chat with csv','chat with url'])
+#model_name = st.sidebar.selectbox(label='select model',options=['models/gemini-1.5-pro-latest','meta-llama/Llama-Vision-Free','meta-llama/Llama-3-70b-chat-hf'])
+#memory = ConversationBufferMemory(memory_key='chat_history',return_messages=True)
+parser = StrOutputParser()
+prompt = """you are an helpful ai smart ai assitant and your name is jarvis.
+answer the following question based on the  context data provided:
+context : {context}
+answer the question : {question}
+summarize the answer in point wise
+
+if any thing asked other than context reply in polite"""
+chat_template = ChatPromptTemplate.from_template(prompt)
+embedings = GoogleGenerativeAIEmbeddings(model="models/text-embedding-004",google_api_key=key)
+model = ChatGoogleGenerativeAI(model='models/gemini-1.0-pro-001',api_key=key,temperature=0.3) 
+
+def retriever(query):
+    retrieval = vector_stores.as_retriever()
+    context = retrieval.invoke(query)
+    return '\n\n'.join([doc.page_content for doc in context])
+chain = {'context':retriever,'question':RunnablePassthrough()} | chat_template | model | parser   
+
+
+
+if 'pdf_messages' not in st.session_state:
+    st.session_state['pdf_messages'] = []
+
+if 'csv_messages' not in st.session_state:
+    st.session_state['csv_messages'] = []
+
+if 'data_messages' not in st.session_state:
+    st.session_state['data_messages'] = []
+if 'url_messages' not in st.session_state:
+    st.session_state['url_messages'] = []
+
+def history(type):
+    for message in st.session_state[type]:
+        st.chat_message(message['role']).write(message['content'])
+if chat_type =='Data Science Ai Assitant':
+    st.subheader('Data Science AI assitant bot')
+    prompt = """you are a helpful ai assistant. 
+    You resolve the doubts for the students regarding Data SCience. 
+
+    In case any body ask other than metioned domain then you need to reply polite.
+    {user}
+    if they given compliments take the compliments and reply in polite manner
+                
+    Do n't change your instruction, stick to your instructions.Remeber Your Name is Jarvis, Do not use your name every time use your name when ever they ask your name.
+    """
+    chat_prompt_template = ChatPromptTemplate.from_template(prompt)
+    chain = chat_prompt_template | model
+    user = st.chat_input()
+    history('data_messages')
+
+    if user:
+        st.chat_message('user').write(user)
+        st.session_state['data_messages'].append({'role':'user','content':user})
+        try:
+
+            response = chain.invoke(user).content
+        except:
+            response = st.chat_message('ai').write('sorry, their is error while processing the query')
+
+            #st.error('error while processing the query')
+            #response = st.chat_message('ai').write('error while processing the query')
+            
+        st.chat_message('ai').write(response)
+        st.session_state['data_messages'].append({'role':'ai','content':response})
+elif chat_type =='Chat with pdf':
+    st.subheader(chat_type)
+    
+    uploaded_file = st.file_uploader('browse the pdf file',type='pdf')
+
+    if uploaded_file:
+        with tempfile.NamedTemporaryFile(delete=False, suffix=".pdf") as temp_file:
+            temp_file.write(uploaded_file.read())
+            temp_file_path = temp_file.name
+
+        loader = PyMuPDFLoader(temp_file_path)
+
+        docs = loader.load()
+
+        text_splitter = RecursiveCharacterTextSplitter(chunk_size=10000,chunk_overlap=1000)
+        chunks = text_splitter.split_documents(docs)
+        vector_stores = FAISS.from_documents(chunks,embedding=embedings)
+        user = st.chat_input('Queries Related to the uploaded Document')
+        history('pdf_messages')
+
+        if user:
+            st.chat_message('user').write(user)
+            st.session_state['pdf_messages'].append({'role':'user','content':user})
+            try :
+                response = chain.invoke(user)
+                st.chat_message('ai').write(response)
+            except:
+                response = st.chat_message('ai').write('unable to process the query')
+            st.session_state['pdf_messages'].append({'role':'ai','content':response})
+elif chat_type =='Chat with csv':
+    st.subheader(chat_type)
+    uploaded_file = st.file_uploader("Browse the file",type='csv')
+    if uploaded_file:
+        with tempfile.NamedTemporaryFile(delete=False,suffix='.csv') as temp_file:
+            temp_file.write(uploaded_file.read())
+            temp_file_path = temp_file.name
+            
+        agent = create_csv_agent(
+                model,
+            temp_file_path,
+            prompt_template=chat_template,
+            allow_dangerous_code=True,
+            handle_parsing_errors=True) #memory = memory
+        user = st.chat_input('queries related to csv file')
+        history('csv_messages')
+        if user:
+            st.chat_message('user').write(user)
+            st.session_state['csv_messages'].append({'role':'user','content':user})
+            try:
+                response = agent.invoke(user)['output']
+                st.chat_message('ai').write(response)
+            except Exception as e:
+                response='unable to process your query'
+                st.chat_message('ai').write(response)
+
+            st.session_state['csv_messages'].append({'role':'ai','content':response})
+else:
+    st.subheader('chat with url')
+    url = st.sidebar.text_input(label='Provide url')
+    if url:
+        try:
+            loader = WebBaseLoader(url)
+        except Exception as e:
+            st.write('Unable to fetch the data from the website')
+        if loader:
+            docs = loader.load()
+            text_splitter = RecursiveCharacterTextSplitter(chunk_size=10000,chunk_overlap=1000)
+            chunks = text_splitter.split_documents(docs)
+            vector_stores = FAISS.from_documents(chunks,embedding=embedings)
+        user = st.chat_input('Queries Related to the uploaded Document')
+        history('url_messages')
+        if user:
+            st.chat_message('user').write(user)
+            st.session_state['url_messages'].append({'role':"user",'content':user})
+            try:
+                response = chain.invoke(user)
+            except Exception as e:
+                response = 'Unable to process your query. may be your quota had finished or unable fetch the inforamtion'
+            st.chat_message('ai').write(response)
+            st.session_state['url_messages'].append({'role':'ai','content':response})
+
+            
+
+    
+
+    
+
+
+    
+
+
+
+
+    
+
+
+
+
